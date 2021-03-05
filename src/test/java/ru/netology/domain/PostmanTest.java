58cc87e518bcb6bb4e635403f095c1971ce254d9
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-03-05 21:12:41 +0300

    bild gradle

--- src/test/java/ru/netology/domain/PostmanTest.java
+++ src/test/java/ru/netology/domain/PostmanTest.java
@@ -9,6 +9,7 @@ class PostmanTest {
 
     @Test
     void shouldReturnDemoAccounts() {
+
         given()// Предусловия
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
